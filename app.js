12914e1ebf8d3a50e98d121e85cfdc085dfd6f8e
Matto <matt.mcquain@gmail.com>
2019-08-27 20:23:14 -0700

    some initial work on the https module

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
+// We need to get the required https module first
+const https = require('https');
+const username = "chalkers";
+
+// Function to print message to console
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
+}
+
 // Connect to the API URL (https://teamtreehouse.com/username.json)
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+  console.dir(response); // this will show us all possible properties that are available to us, but its a bit much.
+ 
+  // console.log(response.statusCode); will give us the status code...obviously
+
+})
+
 // Read the data
 // Parse the data
 // Print the data
