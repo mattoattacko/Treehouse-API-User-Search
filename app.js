aa19185a0e24724f4d9c1e04fdcc8ce4e291a418
Matto <matt.mcquain@gmail.com>
2019-08-29 19:29:35 -0700

    refactored code to allow for command line arguments

--- app.js
+++ app.js
@@ -3,37 +3,41 @@
 
 // We need to get the required https module first
 const https = require('https');
-const username = "chalkers";
 
 // Function to print message to console
 function printMessage(username, badgeCount, points) {
-  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-  console.log(message);
+	const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+	console.log(message);
 }
 
-// Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-  // console.dir(response); this will show us all possible properties that are available to us, but its a bit much
-  // console.log(response.statusCode); will give us the status code
 
-  let body = "";
+function getProfile(username) {
+	// Connect to the API URL (https://teamtreehouse.com/username.json)
+	const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
 
-  // Read the data
-  response.on('data', data => {
-   body += data.toString();
-  });
+		let body = "";
 
-  response.on('end', () => {
-    // Parse the data
-    // Store the object as a constant variable called profile
-    const profile = JSON.parse(body);
-    // We can use the dir output to figure out how many badges and points we have 
-    // console.dir(profile);
+		// Read the data
+		response.on('data', data => {
+			body += data.toString();
+		});
 
-    // we have a badges array, so we can print a message to show our badge count. There is a points object segmented into topic areas. 
-    // Print the data
-    printMessage(username,profile.badges.length, profile.points.JavaScript);
-  });
+		response.on('end', () => {
+			// Parse the data
+			// Store the object as a constant variable called profile
+			const profile = JSON.parse(body);
+			// We can use the dir output to figure out how many badges and points we have 
+			// console.dir(profile);
 
+			// we have a badges array, so we can print a message to show our badge count. There is a points object segmented into topic areas. 
+			// Print the data
+      printMessage(username, profile.badges.length, 
+        profile.points.JavaScript);
+		});
+	});
+}
 
-});
+const users = process.argv.slice(2);
+users.forEach(getProfile);
+
+// Unsure of cause of bug that caused getProfiles to not be read. Removing the commented code that was here fixes that problem. Nebulous as to reasoning...
